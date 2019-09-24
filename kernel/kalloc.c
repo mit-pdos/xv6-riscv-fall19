9eb5f3179b4d3733a16731f3d60d57810c6b7929
Josh Fried <joshuafried@gmail.com>
2019-09-24 00:09:22 -0400

    switch back to old allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,13 +9,34 @@
 #include "riscv.h"
 #include "defs.h"
 
+void freerange(void *pa_start, void *pa_end);
+
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
 void
 kinit()
 {
-  char *p = (char *) PGROUNDUP((uint64) end);
-  bd_init(p, (void*)PHYSTOP);
+  initlock(&kmem.lock, "kmem");
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    kfree(p);
 }
 
 // Free the page of physical memory pointed at by v,
@@ -25,7 +46,20 @@ kinit()
 void
 kfree(void *pa)
 {
-  bd_free(pa);
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -34,5 +68,15 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  return bd_malloc(PGSIZE);
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
 }
