9dbb2fad58aea0a5b24a448b11406718fc484f3d
Robert Morris <rtm@csail.mit.edu>
2019-10-26 05:12:02 -0400

    NPROC=10 for faster bigfile

--- kernel/param.h
+++ kernel/param.h
@@ -1,4 +1,4 @@
-#define NPROC        64  // maximum number of processes
+#define NPROC        10  // maximum number of processes
 #define NCPU          8  // maximum number of CPUs
 #define NOFILE       16  // open files per process
 #define NFILE       100  // open files per system
