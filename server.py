856b4ffc2ed2e5d75c022f2a073f830cb8d13004
Adam Belay <adam@abelay.me>
2019-11-14 18:42:47 +0000

    Remove port number instructions from server (now nettests discovers the port number at compile time).

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = ('localhost', int(sys.argv[1]))
 print >>sys.stderr, 'listening on %s port %s' % addr
-print >>sys.stderr, 'provide this port number as an arg to nettests inside xv6'
 sock.bind(addr)
 
 while True:
