7b436d2897ea21176566f332bb9206a97a426808
coldwater <clatisus@gmail.com>
2020-02-15 23:41:50 +0800

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if(argc != 2) {
+        fprintf(2, "usage: sleep seconds\n");
+        exit();
+    }
+
+    sleep(atoi(argv[1]));
+    exit();
+}
