a8bcff9fe083d04f6f5bef0a51413428576b5b4b
coldwater <clatisus@gmail.com>
2020-02-16 16:22:02 +0800

    find

--- Makefile
+++ Makefile
@@ -143,6 +143,7 @@ UPROGS=\
 	$U/_sleep\
 	$U/_pingpong\
 	$U/_primes\
+	$U/_find\
 
 fs.img: mkfs/mkfs README user/xargstest.sh $(UPROGS)
 	mkfs/mkfs fs.img README user/xargstest.sh $(UPROGS)
---
+++ user/find.c
@@ -0,0 +1,110 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+int match(char*, char*);
+
+// extract filename out of path
+char*
+extname(char *path)
+{
+    char *p;
+    // Find first character after last slash.
+    for(p=path+strlen(path); p >= path && *p != '/'; p--)
+        ;
+    return ++p;
+}
+
+void
+find(char *path, char *re)
+{
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if((fd = open(path, 0)) < 0){
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    switch(st.type){
+    case T_FILE:
+        if(match(re, extname(path))){
+            printf("%s\n", path);
+        }
+        break;
+
+    case T_DIR:
+        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+            printf("find: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf+strlen(buf);
+        *p++ = '/';
+        // iterate all subfiles
+        while(read(fd, &de, sizeof de) == sizeof de){
+            // dirent is free if inum is zero
+            if(de.inum == 0){
+                continue;
+            }
+            // skip . and ..
+            if(!strcmp(de.name, ".") || !strcmp(de.name, "..")){
+                continue;
+            }
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            if(stat(buf, &st) < 0){
+                printf("find: cannot stat %s\n", buf);
+                continue;
+            }
+            find(buf, re);
+        }
+        break;
+    }
+    close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+    if(argc < 3){
+        fprintf(2, "usage: find root_path pattern\n");
+        exit();
+    }
+
+    find(argv[1], argv[2]);
+    exit();
+}
+
+int matchstar(int, char*, char*);
+
+// match: implicitly wrapped by ^$
+int match(char *re, char *text)
+{
+    if(re[0] == '\0' && text[0] == '\0')
+        return 1;
+    if(re[1] == '*')
+        return matchstar(re[0], re+2, text);
+    if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+        return match(re+1, text+1);
+    return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+    do{  // a * matches zero or more instances
+        if(match(re, text))
+            return 1;
+    }while(*text!='\0' && (*text++==c || c=='.'));
+    return 0;
+}
