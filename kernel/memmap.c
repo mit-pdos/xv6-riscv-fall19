1b970573e094b9f1f56dec0fe575ab38791c9dc2
oj <846031220@qq.com>
2020-11-27 10:20:23 +0800

    ?

---
+++ kernel/memmap.c
@@ -0,0 +1,14 @@
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "riscv.h"
+#include "spinlock.h"
+#include "proc.h"
+#include "defs.h"
+
+uint64 sys_mmap(void)
+{
+
+}
+
+uint64 sys_munmap(void)
